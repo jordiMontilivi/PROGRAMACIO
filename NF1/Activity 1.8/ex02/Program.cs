f455e9971766c961bde72e1f3ea591cf17a2ca7b
Jordi Fons <jordifons@institutmontilivi.cat>
2024-11-28 13:01:31 +0100

    afegit ex02 i modificat ex01

---
+++ NF1/Activity 1.8/ex02/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace ex02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables
+            int numAct, numAnt;
+            const string path = @"..\..\..\..\FITXERS FINESTRES\";
+            bool creixent = true;
+            string fitxer;
+            string linia;
+
+            Console.Write("Escriu el nom d'un fitxer: ");
+            fitxer = Console.ReadLine() + ".txt";
+
+            if (File.Exists(path + fitxer))
+            {
+                StreamReader sr = new StreamReader(path + fitxer);
+
+                //Valors entrada 
+                //Correctament hem de controlar que el fitxer no estigui buit o tindrem errors
+                numAnt = Convert.ToInt32(sr.ReadLine());
+                linia = sr.ReadLine();
+                numAct = Convert.ToInt32(linia);
+
+                while (linia != null && !creixent)
+                {
+                    numAct = Convert.ToInt32(linia);
+                    if (numAnt >= numAct)
+                        creixent = false;
+                    else
+                    {
+                        numAnt = numAct;
+                        linia = sr.ReadLine();
+                    }
+                }
+                if(creixent)
+                    Console.WriteLine($"La seqüència de valors és CREIXENT");
+                else
+                    Console.WriteLine($"La seqüència de valors NO ES CREIXENT");
+            }
+            else
+                Console.WriteLine($"No s'ha trobat el fitxer {fitxer} a la ruta {path}");
+        }
+    }
+}
---
+++ NF1/Activity 1.8/ex02b/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ex02b
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables
+            int numAct, numAnt;
+            const string path = @"..\..\..\..\FITXERS FINESTRES\";
+            string fitxer;
+            string linia;
+
+            Console.Write("Escriu el nom d'un fitxer: ");
+            fitxer = Console.ReadLine() + ".txt";
+
+            if (File.Exists(path + fitxer))
+            {
+                StreamReader sr = new StreamReader(path + fitxer);
+
+                //Valors entrada
+                numAnt = Convert.ToInt32(sr.ReadLine());
+                linia = sr.ReadLine();
+
+                while (linia != null && (numAct = Convert.ToInt32(linia)) < numAct)
+                {
+                    numAnt = numAct;
+                    linia = sr.ReadLine();
+                }
+                if (linia == null)
+                    Console.WriteLine($"La seqüència de valors és CREIXENT");
+                else
+                    Console.WriteLine($"La seqüència de valors NO ES CREIXENT");
+            }
+            else
+                Console.WriteLine($"No s'ha trobat el fitxer {fitxer} a la ruta {path}");
+        }
+    }
+}
