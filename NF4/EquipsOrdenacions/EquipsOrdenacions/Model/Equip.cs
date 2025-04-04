70f7841d6f74fe8ce731f614dce6c8eeb6d46a34
Jordi Fons <jordi.fons@institutmontilivi.cat>
2025-04-04 22:24:02 +0200

    Ordenat pero massa calculs de la variable espais

---
+++ NF4/EquipsOrdenacions/EquipsOrdenacions/Model/Equip.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquipsOrdenacions.Model
+{
+	public class Equip : IComparable<Equip>
+	{
+		#region Atributs
+		private string nom;
+		private int golsF;
+		private int golsC;
+		private int punts;
+		#endregion
+
+		#region Propietats
+		public string Nom
+		{
+			get => nom;
+			set => nom = value;
+		}
+		public int GolsF
+		{
+			get => golsF; 
+			set => golsF = value; 
+		}
+		public int GolsC
+		{
+			get { return golsC; }
+			set { golsC = value; }
+		}
+		public int Punts
+		{
+			get { return punts; }
+			set { punts = value; }
+		}
+		#endregion
+
+		#region Constructors
+		public Equip() : this("", 0, 0, 0)
+		{}
+		public Equip(string nom, int golsF, int golsC, int punts)
+		{
+			this.nom = nom;
+			this.golsF = golsF;
+			this.golsC = golsC;
+			this.punts = punts;
+		}
+		public Equip(string csv)
+		{
+			string[] valors;
+			valors = csv.Split(';');
+
+			if (valors.Length != 4)
+				throw new ArgumentException("El CSV ha de tenir 4 arguments");
+
+			nom = valors[0];
+			golsF = Convert.ToInt32(valors[1]);
+			golsC = Convert.ToInt32(valors[2]);
+			punts = Convert.ToInt32(valors[3]);
+		}
+		#endregion
+
+		#region Metodes Object
+		public override string ToString()
+		{
+			return $"{nom};{golsF};{golsC};{punts}";
+		}
+		public override bool Equals(object? other)
+		{
+			bool res = true;
+			Equip otherEquip;
+
+			if (other == null || other is not Equip)
+				res = false;
+			else
+			{
+				otherEquip = other as Equip;
+				if (otherEquip.Nom != nom)
+					res = false;
+			}
+
+			return res;
+		}
+		#endregion
+
+		#region Metodes de Comparacio i nested classes
+		public int CompareTo(Equip? other)
+		{
+			int res;
+			if (other == null)
+				res = 1;
+			else
+				res = nom.CompareTo(other.Nom);
+			return res;
+		}
+
+		//Nested Class per a les comparacions Personalitzades
+		public class ComparadorGolsFavor : IComparer<Equip>
+		{
+			public int Compare(Equip? e1, Equip? e2)
+			{
+				int res;
+				if (e1 == null && e2 == null)
+					res = 0;
+				else if (e1 == null)
+					res = -1;
+				else if (e2 == null)
+					res = 1;
+				else
+					res = e1.GolsF.CompareTo(e2.GolsF); //Si volem ordre descendent haurem de fer un Reverse
+				// Es pot invertir ordre dels comparadors si es volen els gols a favor en ordre descendent
+				//res = e2.GolsF.CompareTo(e1.GolsF);
+
+				return res;
+			}
+		}
+		public class ComparadorGolsContra : IComparer<Equip>
+		{
+			public int Compare(Equip? e1, Equip? e2)
+			{
+				int res;
+				if (e1 == null && e2 == null)
+					res = 0;
+				else if (e1 == null)
+					res = -1;
+				else if (e2 == null)
+					res = 1;
+				else
+					res = e1.GolsC.CompareTo(e2.GolsC);
+
+				return res;
+			}
+		}
+		public class ComparadorPunts : IComparer<Equip>
+		{
+			public int Compare(Equip? e1, Equip? e2)
+			{
+				int res;
+				if (e1 == null && e2 == null)
+					res = 0;
+				else if (e1 == null)
+					res = -1;
+				else if (e2 == null)
+					res = 1;
+				else
+					// Si volem ordre descendent haurem de fer un Reverse o invertir comparació
+					res = e1.Punts.CompareTo(e2.Punts);
+
+				return res;
+			}
+		}
+		#endregion
+	}
+}
---
+++ NF4/EquipsOrdenacions/EquipsOrdenacions/Program.cs
@@ -0,0 +1,116 @@
+﻿using EquipsOrdenacions.Model;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace EquipsOrdenacions
+{
+	internal class Program
+	{
+		//int espais = (Console.WindowWidth - "╔══════════════════════════════════════════╗".Length) / 2 + 5;
+		static void Main(string[] args)
+		{
+			TaulaLlista<Equip> equips = new TaulaLlista<Equip>();
+			string linia;
+			//Amb el using no cal un close del fitxer al final, és més net
+			using (StreamReader sR = new StreamReader("../../../../Equips.csv"))
+			{
+				while ((linia = sR.ReadLine()) != null)
+					equips.Add(new Equip(linia));
+			}
+			Opcions(equips);
+		}
+		public static int MostrarMenu()
+		{
+			int espaisMenu;
+			Centrar("╔══════════════════════════════════════════╗");
+			Centrar("║              PREMIER LEAGE               ║");
+			Centrar("╠══════════════════════════════════════════╣");
+			Centrar("║                                          ║");
+			Centrar("║    1. LLISTAT ALFABÈTIC                  ║");
+			Centrar("║    2. LLISTAT PER CLASSIFICACIÓ          ║");
+			Centrar("║    3. LLISTAT PER GOLS MARCATS           ║");
+			Centrar("║    4. LLISTAT PER GOLS ENCAIXATS         ║");
+			Centrar("║                                          ║");
+			Centrar("║    0. EXIT                               ║");
+			espaisMenu = Centrar("╚══════════════════════════════════════════╝");
+			Console.WriteLine();
+			return espaisMenu;
+		}
+
+		static public void Opcions(TaulaLlista<Equip> equips)
+		{
+			int espaisMenu;
+			ConsoleKeyInfo option;
+			Console.Clear();
+			do
+			{
+				espaisMenu = MostrarMenu();
+				option = Console.ReadKey();
+
+				switch (option.Key)
+				{
+					case ConsoleKey.D1:
+						equips.Sort();
+						MostrarEquips(equips, espaisMenu, "Mostrar Ordenat Alfabèticament");
+						Final(espaisMenu);
+						break;
+					case ConsoleKey.D2:
+						equips.Sort(new Equip.ComparadorPunts());
+						equips.Reverse();
+						MostrarEquips(equips, espaisMenu, "Mostrar Ordenat Per Punts");
+						Final(espaisMenu);
+						break;
+					case ConsoleKey.D3:
+						equips.Sort(new Equip.ComparadorGolsFavor());
+						equips.Reverse();
+						MostrarEquips(equips, espaisMenu, "Mostrar Ordenat Per Gols a Favor");
+						Final(espaisMenu);
+						break;
+					case ConsoleKey.D4:
+						equips.Sort(new Equip.ComparadorGolsContra());
+						MostrarEquips(equips, espaisMenu, "Mostrar Ordenat Per Gols en Contra");
+						Final(espaisMenu);
+						break;
+					case ConsoleKey.D0:
+						for (int i = 5; i >= 0; i--)
+						{
+							Centrar($"Sortint en {i} segons", espaisMenu);
+							Thread.Sleep(1000);
+						}
+						break;
+					default:
+						Centrar($"Opcio no valida", espaisMenu);
+						Final(espaisMenu);
+						break;
+				}
+			} while (option.Key != ConsoleKey.D0);
+		}
+		static public void MostrarEquips(TaulaLlista<Equip> equips, int espais, string titol)
+		{
+			int index = 1;
+			Console.Clear();
+			Console.WriteLine();
+			Centrar(titol, espais);
+			Console.WriteLine("\n");
+			foreach (Equip equip in equips)
+				Centrar($"{index++}. {equip}", espais);
+		}
+		static void Final(int espais)
+		{
+			Console.WriteLine();
+			Centrar("Prem una tecla per continuar: ", espais);
+			Console.ReadKey();
+			Console.Clear();
+		}
+		static int Centrar(string text)
+		{
+			int espais = (Console.WindowWidth - text.Length) / 2;
+			Console.WriteLine($"{new string(' ', espais)}{text}");
+			return espais;
+		}
+		static void Centrar(string text, int espais)
+		{
+			Console.WriteLine($"{new string(' ', espais)}{text}");
+		}
+	}
+}
